405c0d052cf9d167de4661520b958c0112468cec
Dmitry <myxahev2@gmail.com>
2021-01-25 22:43:09 +0500

    complete 5.2

--- 5.2.py
+++ 5.2.py
@@ -1,6 +1,6 @@
 # Создать текстовый файл (не программно), сохранить в нём несколько строк,
 # выполнить подсчёт строк и слов в каждой строке.
 
-print(sum(1 for line in open('out_file.txt', 'r')))
+print(sum(1 for line in open('out_file.txt')))
 
-print(len(open('out_file.txt', 'r').read()))
+print(len(open('out_file.txt').read()))
