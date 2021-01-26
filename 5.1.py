f1fc95a2d805c6e3382567c0130ca2e7f8b2a7ea
Dmitry <myxahev2@gmail.com>
2021-01-26 12:23:09 +0500

    update 5.1

--- 5.1.py
+++ 5.1.py
@@ -1,11 +1,11 @@
 # Создать программный файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
 # Об окончании ввода данных будет свидетельствовать пустая строка.
 
-with open('out_file.txt', 'w', encoding='utf-8') as file_object:
+with open('out_file.txt', 'w', encoding='utf-8') as f:
     while True:
         print('Введите данные и нажмите enter, по окончании введите пустую строку')
         user_answer = input('Введите данные:')
-        file_object.writelines(user_answer + '\n')
+        f.writelines(user_answer + '\n')
         if user_answer == '':
             print('Выход из программы')
             break
