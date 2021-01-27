0b25d14c078d389d49f5d0ab5cea9949b95a4437
Dmitry <myxahev2@gmail.com>
2021-01-27 14:15:26 +0500

    update 5.3

--- 5.3.py
+++ 5.3.py
@@ -6,26 +6,15 @@
 # Иванов 23543.12
 # Петров 13749.32
 
-with open('5.3.txt', encoding='utf-8') as f:
-    count_lines = len(f.readlines())
-    f.seek(0)
-    users = []
-    line = 0
-    i = 1
-    while True:
-        line += 1
-        user = f.readline().split()
-        user[i] = float(user[i])
-        users.append(user)
-        if line == count_lines:
-            all_users = dict(users)
-            break
-    salary = []
-    for key, value in all_users.items():
-        if value <= 20000:
-            salary.append(key)
-
-print('Оклад меньше 20000 у:', salary)
-print('Средний оклад у сотрудников: ', round(sum(list(all_users.values()))) / count_lines)
+with open('5.3.txt', 'r') as f:
+    sal = []
+    people = []
+    my_list = f.read().split('\n')
+    for i in my_list:
+        i = i.split()
+        if int(i[1]) < 20000:
+           people.append(i[0])
+        sal.append(i[1])
+print(f'Оклад меньше 20.000 {people}, средний оклад {sum(map(int, sal)) / len(sal)}')
 
 
