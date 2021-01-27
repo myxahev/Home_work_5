633302a57b635a7a053597804e274ae44cd72c0a
Dmitry <myxahev2@gmail.com>
2021-01-27 18:02:46 +0500

    complete 5.7

--- 5.7.py
+++ 5.7.py
@@ -10,3 +10,32 @@
 # Пример json-объекта:
 # [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 # Подсказка: использовать менеджер контекста.
+
+import json
+profit = {}
+pr = {}
+prof = 0
+prof_aver = 0
+i = 0
+with open('5.7.txt', 'r', encoding='utf-8') as f:
+    for line in f:
+        name, firm, earning, damage = line.split()
+        profit[name] = int(earning) - int(damage)
+        if profit.setdefault(name) >= 0:
+            prof = prof + profit.setdefault(name)
+            i += 1
+    if i != 0:
+        prof_aver = prof / i
+        print(f'Прибыль средняя - {prof_aver:.2f}')
+    else:
+        print(f'Прибыль средняя - отсутсвует. Все работают в убыток')
+    pr = {'средняя прибыль': round(prof_aver)}
+    profit.update(pr)
+    print(f'Прибыль каждой компании - {profit}')
+
+with open('5.7.json', 'w', encoding='utf-8') as f_js:
+    json.dump(profit, f_js)
+
+    js_str = json.dumps(profit)
+    print(f'Создан файл json с содержимым: \n '
+          f' {js_str}')
