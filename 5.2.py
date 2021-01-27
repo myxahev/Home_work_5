516429b2c01207d39ac93fddc2ac0d4fae09d752
Dmitry <myxahev2@gmail.com>
2021-01-27 22:49:27 +0500

    update 5.2

--- 5.2.py
+++ 5.2.py
@@ -1,7 +1,16 @@
 # Создать текстовый файл (не программно), сохранить в нём несколько строк,
 # выполнить подсчёт строк и слов в каждой строке.
 
-with open('out_file.txt') as file:
-    print(sum(1 for line in file))
 
-    print(len(file.read()))
+with open('out_file.txt') as f:
+    count_lines = len(f.readlines())
+    f.seek(0)
+
+    num_line = 0
+    while True:
+        num_line += 1
+        line = len(f.readline().split())
+        print(f'В {num_line} строке {line} слово')
+        if line == 0 or num_line == count_lines:
+            break
+    print(f'Общее количество строк: {count_lines - 1}')
